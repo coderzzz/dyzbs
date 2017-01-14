8063c20ea0f50fd4e4d361222a591f47971f6c98
Interest <interest@InterestdeiMac.local>
2017-01-14 18:25:03 +0800

    init tabbar

--- idyzb/idyzb/BaseVC/TabbarViewController.swift
+++ idyzb/idyzb/BaseVC/TabbarViewController.swift
@@ -12,13 +12,44 @@ class TabbarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "test"
+       
         
-
-        let vc = UIViewController()
+        
+        let item1 = UITabBarItem (title: "首页", image: UIImage(named: "tabbar_home"), selectedImage: UIImage(named: "tabbar_home_selected"))
+        let vc = HomeViewController()
+        vc.tabBarItem = item1;
         vc.view.backgroundColor = UIColor.blueColor()
         let nav = BaseNavgationController(rootViewController:vc)
-        self .addChildViewController(nav)
+        
+        let litme = UITabBarItem()
+        litme.title = "直播"
+        let lvc = LiveViewController()
+        lvc.tabBarItem = litme;
+        lvc.view.backgroundColor = UIColor.yellowColor()
+        let lnav = BaseNavgationController(rootViewController:lvc)
+        
+        let fitme = UITabBarItem()
+        fitme.title = "关注"
+        let fvc = FocusViewController()
+        fvc.tabBarItem = fitme;
+        fvc.view.backgroundColor = UIColor.redColor()
+        let fnav = BaseNavgationController(rootViewController:fvc)
+        
+        let ditme = UITabBarItem()
+        ditme.title = "发现"
+        let dvc = DiscoverViewController()
+        dvc.tabBarItem = ditme;
+        dvc.view.backgroundColor = UIColor.whiteColor()
+        let dnav = BaseNavgationController(rootViewController:dvc)
+        
+        let mitme = UITabBarItem()
+        mitme.title = "我的"
+        let mvc = MineViewController()
+        mvc.tabBarItem = mitme;
+        mvc.view.backgroundColor = UIColor.purpleColor()
+        let mnav = BaseNavgationController(rootViewController:mvc)
+        
+        self.viewControllers = [nav,lnav,fnav,dnav,mnav]
         
         
     }
